286ee8835ae990b15abb236d7ef9766d669692e6
Bruno Travassos <brunnomoraaes@gmail.com>
2022-11-22 00:13:24 -0300

    finalizando o Header

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
-import React from "react";
+import {
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UsersIcon,
+  SearchIcon,
+} from "@heroicons/react/solid";
 
 function Header() {
   return (
-    <header className="sticky top-0 z-50">
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white p-5 shadow-md md:px-10">
       <div className="relative my-auto flex h-10 cursor-pointer items-center">
         <Image
           src="https://links.papareact.com/qd3"
@@ -13,9 +19,24 @@ function Header() {
         />
       </div>
 
-      <div></div>
+      <div className="flex items-center rounded-full py-2 md:border-2 md:shadow-sm">
+        <input
+          className="flex-grow bg-transparent pl-5 text-sm text-gray-600 placeholder-gray-400 outline-none"
+          type="text"
+          placeholder="Start your search"
+        />
+        <SearchIcon className="hidden h-8 cursor-pointer rounded-full bg-red-400 p-2 text-white md:mx-2 md:inline-flex" />
+      </div>
 
-      <div></div>
+      <div className="flex items-center justify-end space-x-4 text-gray-500">
+        <p className="hidden cursor-pointer md:inline">Becoma a host</p>
+        <GlobeAltIcon className="h-6" />
+
+        <div className="flex items-center space-x-2 rounded-full border-2 p-2">
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
     </header>
   );
 }
